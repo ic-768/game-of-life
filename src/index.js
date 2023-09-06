a3f8cdee9ac3e76b2d92f705013d1e7bc6d496b9
Ioannis Chrysostomakis <ic768@protonmail.com>
2023-09-07 00:45:00 +0300

    Update react

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import App from "./App"
+import App from "./App";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+if (!container) throw new Error("container not found!");
+
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-)
+  </React.StrictMode>
+);
